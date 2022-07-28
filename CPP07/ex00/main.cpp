128c8d5b267e640db727937dca8b5423cc7e1ef2
bdetune <25184475+soleraeus@users.noreply.github.com>
2022-07-28 12:44:40 +0200

    Cleaning CPP07

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bdetune <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 17:25:21 by bdetune           #+#    #+#             */
-/*   Updated: 2022/07/22 18:37:24 by bdetune          ###   ########.fr       */
+/*   Updated: 2022/07/28 12:18:55 by bdetune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,10 @@
 
 int	main(void)
 {
-	/*
-	int	a = 42;
-	int b = 1;
-	int const	a1 = -25;
-	int const	b1 = -42;
-	Fixed	c(23.95f);
-	Fixed	d(1000.05f);
-
-	std::cout << "Pre-swap a: " << a << ", b: " << b << std::endl;
-	swap<int>(a, b);
-	std::cout << "Post-swap a: " << a << ", b: " << b << std::endl;
-	std::cout << "Max is: " << max<int>(a, b) << std::endl;
-	std::cout << "Min is: " << min<int>(a, b) << std::endl;
-	std::cout << std::endl;
-
-	std::cout << "Pre-swap a1: " << a1 << ", b1: " << b1 << std::endl;
-	swap<int const>(a1, b1);
-	std::cout << "Post-swap a1: " << a1 << ", b1: " << b1 << std::endl;
-	std::cout << "Max is: " << max<int const>(a1, b1) << std::endl;
-	std::cout << "Min is: " << min<int const>(a1, b1) << std::endl;
-	std::cout << std::endl;
-
-	std::cout << "Pre-swap Fixed c: " << c << ", Fixed d: " << d << std::endl;
-	swap<Fixed>(c, d);
-	std::cout << "Post-swap Fixed c: " << c << ", Fixed d: " << d << std::endl;
-	std::cout << "Max is: " << max<Fixed>(c, d) << std::endl;
-	std::cout << "Min is: " << min<Fixed>(c, d) << std::endl;
-	std::cout << std::endl;
-	*/
 	int a = 2;
 	int b = 3;
 	
+	std::cout << "Tests from subject" << std::endl;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -61,6 +33,26 @@ int	main(void)
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	int const	a1 = -25;
+	int const	b1 = -42;
+	Fixed	e(23.95f);
+	Fixed	f(1000.05f);
+
+	std::cout << std::endl << std::endl << "Custom tests" << std::endl;
+	std::cout << "Pre-swap a1: " << a1 << ", b1: " << b1 << std::endl;
+	::swap<int const>(a1, b1);
+	std::cout << "Post-swap a1: " << a1 << ", b1: " << b1 << std::endl;
+	std::cout << "Max is: " << ::max<int const>(a1, b1) << std::endl;
+	std::cout << "Min is: " << ::min<int const>(a1, b1) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Pre-swap Fixed e: " << e << ", Fixed f: " << f << std::endl;
+	::swap<Fixed>(e, f);
+	std::cout << "Post-swap Fixed c: " << e << ", Fixed d: " << f << std::endl;
+	std::cout << "Max is: " << ::max<Fixed>(e, f) << std::endl;
+	std::cout << "Min is: " << ::min<Fixed>(e, f) << std::endl;
+	std::cout << std::endl;
 
 	return (0);
 }
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,13 +6,14 @@
 /*   By: bdetune <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 17:25:21 by bdetune           #+#    #+#             */
-/*   Updated: 2022/07/22 19:03:28 by bdetune          ###   ########.fr       */
+/*   Updated: 2022/07/28 12:38:43 by bdetune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include "iter.hpp"
+#include "Fixed.hpp"
 
 template<typename T>
 void	square(T & x)
@@ -25,22 +26,46 @@ void	print_minus_1(int x)
 	std::cout << (x-1) << std::endl;
 }
 
+void	increment(Fixed & a)
+{
+	++a;
+	++a;
+}
+
 int	main(void)
 {
 	int	tab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+	std::cout << "Array of int" << std::endl;
 	std::cout << "Array before iter: " << std::endl;
 	for (int i = 0; i < 11; i++)
 	{
-		std::cout << tab[i] << std::endl;
+		std::cout << tab[i] << "  ";
 	}
-	::iter(tab, 11, square<int>);
-	std::cout << "Array after iter: " << std::endl;
+	std::cout << std::endl;
+	::iter(tab, 11, ::square<int>);
+	std::cout << "Array after iter elevating to power of 2 each element: " << std::endl;
 	for (int i = 0; i < 11; i++)
 	{
-		std::cout << tab[i] << std::endl;
+		std::cout << tab[i] << "  ";
 	}
-	std::cout << "With function -1" << std::endl;
-	::iter(tab, 11, print_minus_1);
+	std::cout << std::endl;
+	std::cout << "Second iter printing an int param -1" << std::endl;
+	::iter(tab, 11, ::print_minus_1);
+	std::cout << std::endl;
+
+	std::cout << "Testing with array of fixed" << std::endl;
+	Fixed	fixedArray[10];
+	for (int i = 0; i < 10; i++)
+		fixedArray[i] = Fixed(i) + Fixed(static_cast<float>(i)/10);
+	std::cout << "Fixed array before iter: " << std::endl;
+	for (int i = 0; i < 10; i++)
+		std::cout << fixedArray[i] << "  ";
+	std::cout << std::endl;
+	::iter(fixedArray, 10, ::increment);
+	std::cout << "Fixed array after iter calling twice pre-crement operator: " << std::endl;
+	for (int i = 0; i < 10; i++)
+		std::cout << fixedArray[i] << "  ";
+	std::cout << std::endl;
 	return (0);
 }
